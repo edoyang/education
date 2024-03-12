c3f6fc3f21e0117e72cf931ff17b431bc6aabd24
EDOARDO <K200284@Student.Kent.edu.au>
2024-03-12 16:01:26 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='hero'>
+        <img src='hero.png' alt='hero' />
+        <div className='left'>
+          <h1>The Smart
+            Choice For Future
+          </h1>
+          <p>Elearn is a global training provider based across the UK that specialises in accredited and bespoke training courses. We crush the...</p>
+          <div className='search-bar'>
+            <div className='find'>
+              <img src='mirror.svg' alt='search' />
+              <input type='text' placeholder='Search for a course' />
+            </div>
+            <button className='btn-primary'>Continue</button>
+          </div>
+        </div>
+        <div className='right'>
+          <img src='right.png' alt='right' />
+        </div>
+      </div>
+    </>
   );
 }
 
